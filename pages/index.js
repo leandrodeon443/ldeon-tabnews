6d47a32127ad2afeda1b8b5a36a7fbb9503c30ae
leandrodeon443 <32469555+leandrodeon443@users.noreply.github.com>
2024-11-26 01:00:31 +0000

    html um pouco melhor

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 function Home() {
-    return <h1>Hello World! Teste Push force</h1>
-
+    return (
+        <>
+            <head>
+                <title>LDEON TabNews!</title>
+            </head>
+            <body>
+                <h2>Uma página qualquer!</h2>
+                <h1>Aqui vai um texto qualquer, será que consigo fazer um botão?</h1>
+                <button onClick={() => alert('Botão clicado!')}>Clique aqui</button>
+            </body>
+        </>
+    );
 }
 
 export default Home;
